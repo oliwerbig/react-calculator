dce8fa359ef62713734cfafdb0a9a59021117eb1
Oliver Emanuel Nagy <dev@nagyoliveremanuel.hu>
2020-07-19 23:14:51 +0200

    Updates

---
+++ precache-manifest.80173dbdedc8b7aee2fa8b5a53ff60b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "809fc363393217258f0ffe4ee7f2d646",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "9ed654e5179056e325c2",
+    "url": "/react-calculator/static/css/main.2d260c73.chunk.css"
+  },
+  {
+    "revision": "fc4e8a3b1fd87440e542",
+    "url": "/react-calculator/static/js/2.4cf18fac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator/static/js/2.4cf18fac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ed654e5179056e325c2",
+    "url": "/react-calculator/static/js/main.0e89db7d.chunk.js"
+  },
+  {
+    "revision": "3c4ee116adfa9d8605e9",
+    "url": "/react-calculator/static/js/runtime-main.5a7a9823.js"
+  }
+]);
