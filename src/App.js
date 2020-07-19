03aff0a2966573aa30166680d3520899a0ef152a
Oliver Emanuel Nagy <dev@nagyoliveremanuel.hu>
2020-07-19 17:32:33 +0200

    react-calculator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import Calculator from './layout/Calculator/Calculator'
+
+const app = () => (
+    <div className="app">
+      <Calculator />
     </div>
-  );
-}
+);
 
-export default App;
+export default app;
